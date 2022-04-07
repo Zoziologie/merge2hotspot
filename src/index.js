f06d38b5f8d0757855b5795e90d5380da785dfae
rafnuss <rafnuss@gmail.com>
2022-04-07 11:18:38 -0400

    Add public data + minor fix in js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import $ from "jquery";
 import 'bootstrap';
-import L from 'leaflet';
+import * as L from 'leaflet';
 import 'leaflet-sidebar-v2';
-import 'papaparse';
+import * as Papa from 'papaparse';
 import 'leaflet-makimarkers'
 import 'leaflet-providers'
 import 'leaflet-spin'
@@ -17,62 +17,71 @@ import './style.css';
 //import 'leaflet-makimarkers/MarkerCluster.css;
 
 
-var loc=[], hotspot=[], locNoHot=[], downloaded=[], processed=true, sidebar, makersLoc, makersLocNoHot, makersHotspot
+
 var cntrList = [["AC","Ashmore and Cartier Islands"],["AD","Andorra"],["AE","United Arab Emirates"],["AF","Afghanistan"],["AG","Antigua and Barbuda"],["AI","Anguilla"],["AL","Albania"],["AM","Armenia"],["AO","Angola"],["AQ","Antarctica"],["AR","Argentina"],["AS","American Samoa"],["AT","Austria"],["AU","Australia"],["AW","Aruba"],["AZ","Azerbaijan"],["BA","Bosnia and Herzegovina"],["BB","Barbados"],["BD","Bangladesh"],["BE","Belgium"],["BF","Burkina Faso"],["BG","Bulgaria"],["BH","Bahrain"],["BI","Burundi"],["BJ","Benin"],["BL","Saint Barthélemy"],["BM","Bermuda"],["BN","Brunei"],["BO","Bolivia"],["BQ","Caribbean Netherlands"],["BR","Brazil"],["BS","Bahamas"],["BT","Bhutan"],["BV","Bouvet Island"],["BW","Botswana"],["BY","Belarus"],["BZ","Belize"],["CA","Canada"],["CC","Cocos (Keeling) Islands"],["CD","DR Congo"],["CF","Central African Republic"],["CG","Congo"],["CH","Switzerland"],["CI","Côte d'Ivoire"],["CK","Cook Islands"],["CL","Chile"],["CM","Cameroon"],["CN","China"],["CO","Colombia"],["CP","Clipperton Island"],["CR","Costa Rica"],["CS","Coral Sea Islands"],["CU","Cuba"],["CV","Cape Verde"],["CW","Curaçao"],["CX","Christmas Island"],["CY","Cyprus"],["CZ","Czech Republic"],["DE","Germany"],["DJ","Djibouti"],["DK","Denmark"],["DM","Dominica"],["DO","Dominican Republic"],["DZ","Algeria"],["EC","Ecuador"],["EE","Estonia"],["EG","Egypt"],["EH","Western Sahara"],["ER","Eritrea"],["ES","Spain"],["ET","Ethiopia"],["FI","Finland"],["FJ","Fiji"],["FK","Falkland Islands (Malvinas)"],["FM","Micronesia"],["FO","Faroe Islands"],["FR","France"],["GA","Gabon"],["GB","United Kingdom"],["GD","Grenada"],["GE","Georgia"],["GF","French Guiana"],["GG","Guernsey"],["GH","Ghana"],["GI","Gibraltar"],["GL","Greenland"],["GM","Gambia"],["GN","Guinea"],["GP","Guadeloupe"],["GQ","Equatorial Guinea"],["GR","Greece"],["GS","South Georgia and South Sandwich Islands"],["GT","Guatemala"],["GU","Guam"],["GW","Guinea-Bissau"],["GY","Guyana"],["HK","Hong Kong"],["HM","Heard Island and McDonald Islands"],["HN","Honduras"],["HR","Croatia"],["HT","Haiti"],["HU","Hungary"],["ID","Indonesia"],["IE","Ireland"],["IL","Israel"],["IM","Isle of Man"],["IN","India"],["IO","British Indian Ocean Territory"],["IQ","Iraq"],["IR","Iran"],["IS","Iceland"],["IT","Italy"],["JE","Jersey"],["JM","Jamaica"],["JO","Jordan"],["JP","Japan"],["KE","Kenya"],["KG","Kyrgyzstan"],["KH","Cambodia"],["KI","Kiribati"],["KM","Comoros"],["KN","Saint Kitts and Nevis"],["KP","North Korea"],["KR","South Korea"],["KW","Kuwait"],["KY","Cayman Islands"],["KZ","Kazakhstan"],["LA","Laos"],["LB","Lebanon"],["LC","Saint Lucia"],["LI","Liechtenstein"],["LK","Sri Lanka"],["LR","Liberia"],["LS","Lesotho"],["LT","Lithuania"],["LU","Luxembourg"],["LV","Latvia"],["LY","Libya"],["MA","Morocco"],["MC","Monaco"],["MD","Moldova"],["ME","Montenegro"],["MF","Saint Martin (French part)"],["MG","Madagascar"],["MH","Marshall Islands"],["MK","Macedonia"],["ML","Mali"],["MM","Myanmar"],["MN","Mongolia"],["MO","Macau"],["MP","Northern Mariana Islands"],["MQ","Martinique"],["MR","Mauritania"],["MS","Montserrat"],["MT","Malta"],["MU","Mauritius"],["MV","Maldives"],["MW","Malawi"],["MX","Mexico"],["MY","Malaysia"],["MZ","Mozambique"],["NA","Namibia"],["NC","New Caledonia"],["NE","Niger"],["NF","Norfolk Island"],["NG","Nigeria"],["NI","Nicaragua"],["NL","Netherlands"],["NO","Norway"],["NP","Nepal"],["NR","Nauru"],["NU","Niue"],["NZ","New Zealand"],["OM","Oman"],["PA","Panama"],["PE","Peru"],["PF","French Polynesia"],["PG","Papua New Guinea"],["PH","Philippines"],["PK","Pakistan"],["PL","Poland"],["PM","Saint Pierre and Miquelon"],["PN","Pitcairn Islands"],["PR","Puerto Rico"],["PS","Palestinian Territory"],["PT","Portugal"],["PW","Palau"],["PY","Paraguay"],["QA","Qatar"],["RE","Réunion"],["RO","Romania"],["RS","Serbia"],["RU","Russia"],["RW","Rwanda"],["SA","Saudi Arabia"],["SB","Solomon Islands"],["SC","Seychelles"],["SD","Sudan"],["SE","Sweden"],["SG","Singapore"],["SH","Saint Helena, Ascension, and Tristan da Cunha"],["SI","Slovenia"],["SJ","Svalbard"],["SK","Slovakia"],["SL","Sierra Leone"],["SM","San Marino"],["SN","Senegal"],["SO","Somalia"],["SR","Suriname"],["SS","South Sudan"],["ST","São Tomé and Príncipe"],["SV","El Salvador"],["SX","Sint Maarten"],["SY","Syria"],["SZ","Swaziland"],["TC","Turks and Caicos Islands"],["TD","Chad"],["TF","French Southern and Antarctic Lands"],["TG","Togo"],["TH","Thailand"],["TJ","Tajikistan"],["TK","Tokelau"],["TL","Timor-Leste"],["TM","Turkmenistan"],["TN","Tunisia"],["TO","Tonga"],["TR","Turkey"],["TT","Trinidad and Tobago"],["TV","Tuvalu"],["TW","Taiwan"],["TZ","Tanzania"],["UA","Ukraine"],["UG","Uganda"],["UM","United States Minor Outlying Islands"],["US","United States"],["UY","Uruguay"],["UZ","Uzbekistan"],["VA","Vatican City (Holy See)"],["VC","Saint Vincent and the Grenadines"],["VE","Venezuela"],["VG","Virgin Islands (British)"],["VI","Virgin Islands (U.S.)"],["VN","Vietnam"],["VU","Vanuatu"],["WF","Wallis and Futuna"],["WS","Samoa"],["XK","Kosovo"],["XX","High Seas"],["YE","Yemen"],["YT","Mayotte"],["ZA","South Africa"],["ZM","Zambia"],["ZW","Zimbabwe"]];
+var token = {
+    mapbox : "pk.eyJ1IjoicmFmbnVzcyIsImEiOiIzMVE1dnc0In0.3FNMKIlQ_afYktqki-6m0g",
+    ebird : "vcs68p4j67pt",
+}
+var processed=true;
+var loc = [];
+var hotspot = [];
+var locNoHot = [];
+var downloaded = [];
 
+//Create map   
+var map = new L.Map('map1');
 
-	//Create map   
-	var map = new L.Map('map1');
-	//map.setView(L.latLng(46.57591, 7.84956), 8);
-	map.fitWorld().zoomIn();
-	var baseLayers = {
-		'MapBox': L.tileLayer.provider('MapBox', {id: 'mapbox/streets-v11', accessToken:token.mapbox}).addTo(map),
-		'OpenStreetMap': L.tileLayer.provider('OpenStreetMap.Mapnik'),
-		'Satellite': L.tileLayer.provider('Esri.WorldImagery')
-	};
-	var control = L.control.layers(baseLayers, null, { collapsed: false	}).addTo(map);
-	L.MakiMarkers.accessToken = token.mapbox;
-	var sidebar = L.control.sidebar({container: 'sidebar'}).addTo(map);
+//map.setView(L.latLng(46.57591, 7.84956), 8);
+map.fitWorld().zoomIn();
+var baseLayers = {
+	'MapBox': L.tileLayer.provider('MapBox', {id: 'mapbox/streets-v11', accessToken:token.mapbox}).addTo(map),
+	'OpenStreetMap': L.tileLayer.provider('OpenStreetMap.Mapnik'),
+	'Satellite': L.tileLayer.provider('Esri.WorldImagery')
+};
+var control = L.control.layers(baseLayers, null, { collapsed: false	}).addTo(map);
+L.MakiMarkers.accessToken = token.mapbox;
+var sidebar = L.control.sidebar({container: 'sidebar'}).addTo(map);
 
-	var makersHotspot = L.markerClusterGroup({	
-		showCoverageOnHover:1, 
-		maxClusterRadius:50,
-		iconCreateFunction: function(cluster) {
-			return L.icon({
-				iconUrl: "/assets/hotspot-icon-hotspot-plus_small.png",
-				iconAnchor: [12, 30],
-				popupAnchor: [0, -12],
-			})
-		}
-	});
-	var makersLocNoHot = L.featureGroup();
-	control.addOverlay(makersHotspot.addTo(map),'eBird Hotspot')
-
-	$("#uploadMyEBirdData").change(function(evt) {
-		if (processed){
-			processFile( evt.target.files[0], evt.target.files[0].size )
-		}
-	});
-
-	
-	// Open my data if me in url
-	if ( window.location.search.substring(1).indexOf('me') !== -1 ){
-		map.spin(true);
-		//console.log('loading personal file')
-		$.get("/assets/MyEBirdData.csv", function(data){
-			map.spin(false);
-			processFile(data, data.length) 
+var makersHotspot = L.markerClusterGroup({	
+	showCoverageOnHover:1, 
+	maxClusterRadius:50,
+	iconCreateFunction: function(cluster) {
+		return L.icon({
+			iconUrl: "hotspot-icon-hotspot-plus_small.png",
+			iconAnchor: [12, 30],
+			popupAnchor: [0, -12],
 		})
 	}
+});
+var makersLocNoHot = L.featureGroup();
+control.addOverlay(makersHotspot.addTo(map),'eBird Hotspot')
+
+$("#uploadMyEBirdData").change(function(evt) {
+	if (processed){
+		processFile( evt.target.files[0], evt.target.files[0].size )
+	}
+});
+
+
+// Open my data if me in url
+if ( window.location.search.substring(1).indexOf('me') !== -1 ){
+	map.spin(true);
+	//console.log('loading personal file')
+	$.get("MyEBirdData.csv", function(data){
+		map.spin(false);
+		processFile(data, data.length) 
+	})
+}
 
 	
-	/*var togglechevron = function(e) {
+	var togglechevron = function(e) {
 		$(e.target).prev().find('.fas').toggleClass('fa-chevron-down fa-chevron-up')
 	};
 	$('#instruction1').on('hidden.bs.collapse',togglechevron);
 	$('#instruction1').on('shown.bs.collapse',togglechevron);
 	$('#instruction2').on('shown.bs.collapse',togglechevron);
-	$('#instruction2').on('hidden.bs.collapse',togglechevron);*/
+	$('#instruction2').on('hidden.bs.collapse',togglechevron);
 
 
 function FindLocationHotspot(){
@@ -99,7 +108,7 @@ function FindLocationHotspot(){
 				pop += '<button role="button" class="btn btn-secondary" onclick="OpenmanageLocations(\''+l.checklists[0]+'\')"><i class="fas fa-edit"></i> Merge to/suggest as hotspot</button></p>'
 				var m = L.marker([parseFloat(l.Latitude), parseFloat(l.Longitude)],{
 					icon:L.icon({
-						iconUrl: "/assets/hotspot-icon_perso_small.png",
+						iconUrl: "hotspot-icon_perso_small.png",
 						iconAnchor: [12, 30],
 						popupAnchor: [0, -12],
 					})
@@ -177,7 +186,7 @@ function processFile( file, size ){
 				data.forEach(function(d){
 					if (che.indexOf(d['Submission ID'])<0 && d.Location!=undefined){
 						if (name.indexOf(d.Location)<0){
-							dnew={};
+							var dnew={};
 							dnew.Location = d.Location;
 							dnew.Latitude = d.Latitude;
 							dnew.Longitude = d.Longitude;
@@ -192,7 +201,8 @@ function processFile( file, size ){
 					}
 				})
 
-				makersLoc = L.markerClusterGroup({	
+				//var makersLoc = L.LayerGroup();
+				var makersLoc = L.markerClusterGroup({	
 					showCoverageOnHover:1,
 					iconCreateFunction: function(e){
 						var t=e.getAllChildMarkers().reduce( (c,m) => c+m.checklists.length,0)
@@ -207,7 +217,7 @@ function processFile( file, size ){
 						return divi
 					}
 				});
-
+				
 				loc.forEach(function(l){
 					var pop = '<p><b>Name:</b> '+l.Location+' ('+l.region+')<br><b>Checklists: </b>'+ l.checklists.map( val => '<a href="https://ebird.org/view/checklist/'+val+'" target="_blank">'+val+'</a>' ).join(", ")+'</p>';
 					var m = L.marker([parseFloat(l.Latitude), parseFloat(l.Longitude)],{
@@ -222,9 +232,9 @@ function processFile( file, size ){
 					makersLoc.addLayer(m);
 				})
 
+				makersLoc.addTo(map)
 				control.addOverlay(makersLoc.addTo(map),'All Locations')
 				map.fitBounds(makersLoc.getBounds(), {paddingTopLeft: [500, 0]});
-
 				ListRegion(loc)
 
 				map.spin(false);
@@ -265,12 +275,12 @@ var ListRegion = function(loc){
 	}, [] )
 
 	country.forEach(function(c){
-		html = '<div class="list-group-item "><a href="#list-country-'+c.locID+'" class="list-crl-a" data-toggle="collapse"><i class="fas fa-chevron fa-chevron-right"></i><span class="chevron-margin">'+c.name+'</span></a><a href="#" class="rcl-download" id="rcl-download-'+c.locID+'"><i class="fas fa-download" ></i></a><span class="badge bg-primary bg-pill">'+c.count.toString()+'</span></div>\
+		var html = '<div class="list-group-item "><a href="#list-country-'+c.locID+'" class="list-crl-a" data-toggle="collapse"><i class="fas fa-chevron fa-chevron-right"></i><span class="chevron-margin">'+c.name+'</span></a><a href="#" class="rcl-download" id="rcl-download-'+c.locID+'"><i class="fas fa-download" ></i></a><span class="badge bg-primary bg-pill">'+c.count.toString()+'</span></div>\
 		<div class="list-group collapse" id="list-country-'+c.locID+'"></div>';
 		$('#list-countrregionlist').append(html)
 		region.forEach(function(r){
 			if(r.locID.split('-')[0]==c.locID){
-				html = '<div class="list-group-item"><span id="locID-'+r.locID+'">'+r.locID+'</span></a>\
+				var html = '<div class="list-group-item"><span id="locID-'+r.locID+'">'+r.locID+'</span></a>\
 				<a href="#" class="rcl-download" id="rcl-download-'+r.locID+'"><i class="fas fa-download"></i></a>\
 				<span class="badge badge-primary badge-pill">'+r.count.toString()+'</span></div>';
 				$('#list-country-'+c.locID).append(html)
@@ -302,12 +312,12 @@ var ListRegion = function(loc){
 		$('#list-region-'+l.region).append(html)
 	})*/
 
-	setTimeout(function(){
-		$('.list-crl-a').on('click', function() {
-			$('.fa-chevron', this)
-			.toggleClass('fa-chevron-right')
-			.toggleClass('fa-chevron-down');
-		});
+	$('.list-crl-a').on('click', function() {
+		$('.fa-chevron', this)
+		.toggleClass('fa-chevron-right')
+		.toggleClass('fa-chevron-down');
+	});
+
 		$('.rcl-download').on('click', function(e) {
 			e.preventDefault();
 			if (!$(this).hasClass('fa-disabled')){
@@ -327,12 +337,14 @@ var ListRegion = function(loc){
 					});
 					$($($("[id^=list-country-"+val+"]")[0]).find('.rcl-download')).addClass('fa-disabled');
 					var hlocid = hotspot.map( h => h.locId);
+					console.log(data)
 					data.forEach(function(d){
+						console.log(d.lat)
 						if (hlocid.indexOf(d.locId)<0){
-							h=d;
+							var h=d;
 							var m = L.marker([parseFloat(d.lat), parseFloat(d.lng)],{
 								icon:L.icon({
-									iconUrl: "/assets/hotspot-icon-hotspot_small.png",
+									iconUrl: "hotspot-icon-hotspot_small.png",
 									iconAnchor: [12, 30],
 									popupAnchor: [0, -12],
 								})
@@ -348,8 +360,6 @@ var ListRegion = function(loc){
 				})
 			}
 		});
-	},0)
-	
 }
 
 
