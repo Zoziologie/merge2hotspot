c4026888e0fe5e4ce31287f73dfbc6bb9767d516
rafnuss <rafnuss@gmail.com>
2022-04-07 11:18:06 -0400

    Fix for webpack inject

--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,8 @@ module.exports = {
   },
   plugins: [
         new HtmlWebpackPlugin({
-          template: 'src/index.html'
+          template: 'src/index.html',
+          inject: false,
         })
     ]
 };
